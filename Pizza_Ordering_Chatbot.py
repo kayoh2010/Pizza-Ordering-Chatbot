1decc87d34cfb2e55f55c1ab4590e9ff738ab7c4
katelynn22 <katelynn.ouellette15@gmail.com>
2022-12-18 10:23:05 -0800

    finishing touches

--- Pizza_Ordering_Chatbot.py
+++ Pizza_Ordering_Chatbot.py
@@ -7,37 +7,60 @@ if userName.lower() == 'katelynn':
     print('\nMy creator, ' + userName + '. Pleasure to serve you!')
 else: 
     print('\nHello, ' + userName + '.  Nice to meet you!')
-size = input("\nWhat size do you want? Enter small, medium, or large:  ")
-while size.lower() != "small" and size.lower != "medium" and size.lower() != "large":
-    size = input("Invalid value.  Please enter small, medium, or large:  ")
-flavor = input("\nEnter the flavor of pizza:  ")
-crustType = input("\nWhat type of crust do you want:  ")
-quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
-while not quantity.isdigit():
-    quantity = input("\nValue not recognized.  Please enter a numeric value:  ")
-quantity = int(quantity)
-method = input("\nIs this carry out or delivery:  ")
-while method.lower() != "delivery" and method.lower() != "carry out" and method.lower()!= "carryout":
-    method = input("\nInvalid value. Please enter delivery or carry out:  ")
-salesTax = 1.1
-if size.lower() == "small":
-    pizzaCost = 8.99
-elif size.lower() == "medium":
-    pizzaCost = 14.99
-elif size.lower() == "large":
-    pizzaCost = 17.99
 
-if method.lower() == "delivery":
-    deliveryFee = 5
-else:
-    deliveryFee = 0
-total = (pizzaCost * quantity) * salesTax + deliveryFee
+keepGoing = "y"
+while keepGoing.lower() == "y":
+    size = input("\nWhat size do you want? Enter small, medium, or large:  ")
+    while size.lower() != "small" and size.lower() != "medium" and size.lower() != "large":
+        size = input("Invalid value.  Please enter small, medium, or large:  ")
 
-if total >= 50:
-    print("\nCongradulations!! You've been rewarded a $10 off coupon for your next order!")
-else:
-    print("\nOrders over $50 will recieve a free $10 off coupon to use on your next order.")
-print("\n---------------")
-print("Thank you,", userName, ", for your order.")
-print("Your",quantity, size, flavor, "pizza with", crustType, "crust costs" , "${:,.2f}" .format(total) + ".")
-print("---------------")
+
+    flavor = input("\nEnter the flavor of pizza:  ")
+    crustType = input("\nWhat type of crust do you want:  ")
+    quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
+    while not quantity.isdigit():
+        quantity = input("\nValue not recognized.  Please enter a numeric value:  ")
+    quantity = int(quantity)
+
+
+    method = input("\nIs this carry out or delivery:  ")
+    while method.lower() != "delivery" and method.lower() != "carry out" and method.lower()!= "carryout":
+        method = input("\nInvalid value. Please enter delivery or carry out:  ")
+    salesTax = 1.1
+    if size.lower() == "small":
+        pizzaCost = 8.99
+    elif size.lower() == "medium":
+        pizzaCost = 14.99
+    elif size.lower() == "large":
+        pizzaCost = 17.99
+
+    if method.lower() == "delivery":
+        deliveryFee = 5
+    else:
+        deliveryFee = 0
+    total = (pizzaCost * quantity) * salesTax + deliveryFee
+
+    if total >= 50:
+        print("\nCongradulations!! You've been rewarded a $10 off coupon for your next order!")
+    else:
+        print("\nOrders over $50 will recieve a free $10 off coupon to use on your next order.")
+    print("\n---------------")
+    print("Thank you,", userName, ", for your order.")
+    print("Your",quantity, size, flavor, "pizza with", crustType, "crust costs" , "${:,.2f}" .format(total) + ".")
+    print("---------------")
+
+    print("Order has been recieved ETA 3 minutes!")
+
+    for min in range(3, 1, -1):
+        print(min, "minutes remaining")
+        for seconds in range(60, 0, -1):
+            print(seconds, end = " \r")
+            import time 
+            time.sleep(1)
+
+    print('Your order is ready!')
+    
+    keepGoing = input('Do you want to place another order? Enter y or n:  ')
+
+
+
